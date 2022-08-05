eab322102b4820b3194da568898de76b145674f1
AmadeusDias <amadeusdias@hotmail.com>
2022-08-05 16:14:54 -0300

    corrigido denovo

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 export async function getCategories() {
- const ENDPOINT= "https://api.mercadolibre.com/sites/MLB/categories";
- const response = await fetch(ENDPOINT);
- const data = await response.json();
- return data 
+  const ENDPOINT = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(ENDPOINT);
+  const data = await response.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery( categoryId, query ) {
-  const ENDPOINT2= `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const ENDPOINT2 = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const response = await fetch(ENDPOINT2);
   const data = await response.json();
   return data;
