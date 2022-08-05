87e7ba58075d549b54ca4d10afb4e8a38abcb6e7
AmadeusDias <amadeusdias@hotmail.com>
2022-08-05 17:08:58 -0300

    requisito 04

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  async componentDidMount() {
+    const data = await getCategories();
+    this.setState({
+      categories: data,
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <Link data-testid="shopping-cart-button" to="/cart">Cart</Link>
@@ -16,6 +32,16 @@ class Home extends React.Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
 
         </p>
+        {categories.map((category) => (
+          <button
+            key={ category.id }
+            type="button"
+            data-testid="category"
+          >
+            {category.name}
+
+          </button>
+        ))}
       </div>
     );
   }
